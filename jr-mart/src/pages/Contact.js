5cf2205cf0232966ed81185e5916fc73f2bbc969
Juned <79745275+junaid11P@users.noreply.github.com>
2025-02-16 21:46:17 +0530

    contact updated

--- jr-mart/src/pages/Contact.js
+++ jr-mart/src/pages/Contact.js
@@ -2,7 +2,12 @@ export default function Contact() {
     return (
         <div className="container my-4">
             <h2>Contact Us</h2>
-            
+            <br />
+            <a href="https://mail.google.com/mail/u/0/?tab=rm&ogbl#inbox?compose=CllgCJlKFmVnzLnDqwlVhjJWdLsvTFgZLJFhWMPwzWCsNXXpHHpnwLLSfPKdWRmGTCZRPDsXWpL" target="_blank" rel="noopener noreferrer"> <img src="/EmailLogo.svg" alt="..." width="30" className="me-2" />JRcontact@gmail.com </a><br />
+            <br/>
+            <a href="https://www.instagram.com/junaid11_" target="_blank" rel="noopener noreferrer"><img src="/instagramLogo.svg" alt="..." width="30" className="me-2" />Junaid11_</a><br /> 
+
+
         </div>
     )   
 }
