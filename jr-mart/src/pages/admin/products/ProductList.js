5451a85887b4eb52c585a32d6f8ec4aa42e1021a
Juned <79745275+junaid11P@users.noreply.github.com>
2025-02-19 00:20:12 +0530

    refresh

--- jr-mart/src/pages/admin/products/ProductList.js
+++ jr-mart/src/pages/admin/products/ProductList.js
@@ -30,16 +30,16 @@ useEffect(getProducts, []);
 
       <div className="row mb-3">
         <div className="col">
-            <Link className="btn btn-primary me-1" href="/admin/products/create" role="button">Create Product</Link>
+            <Link className="btn btn-primary me-1" to="/admin/products/create" role="button">Create Product</Link>
             <button type="button" className="btn btn-outline-primary"
-            onclick={getProducts}>Refresh</button>
+            onClick={getProducts}>Refresh</button>
         </div>
         <div className="col">
 
         </div>
       </div>
       <table className="table">
-        <thread>
+        <thead>
             <tr>
                 <th>ID</th>
                 <th>Name</th>
@@ -49,7 +49,7 @@ useEffect(getProducts, []);
                 <th>Created At</th>
                 <th>Actions</th>
             </tr>
-        </thread>
+        </thead>
         <tbody>
             {
                 products.map((product, index) => {
@@ -59,7 +59,12 @@ useEffect(getProducts, []);
                             <td>{product.name}</td>
                             <td>{product.category}</td>
                             <td>{product.price}$</td>
-                            <td><img src={ "http://localhost:3000/images/" + product.imageFilename} width="100" alt=".."/></td>
+                            <td>
+                                <img 
+                                    src={"http://localhost:3000/images/" + product.imageFilename}
+                                    width="80" alt="..."
+                                />
+                            </td>
                             <td>{product.createdAt.slice(0,10)}</td>
                             <td style={{width: "10px",whiteSpace: "nowrap"}}>
                                 <Link className='btn btn-primary btn-sm me-1'
@@ -70,7 +75,6 @@ useEffect(getProducts, []);
                     )
                 })
             }
-
         </tbody>
         </table>
         
